a9077678282d3147bc7af843041252d719007b2f
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-07-04 10:24:31 +0530

    strong attack handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,6 @@
 const ATTACK_VALUE = 10;
-const MONSTER_ATTACK_VALUE = 12;
 const STRONG_ATTACK_VALUE = 17;
-
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,34 +8,33 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode){
-    if (mode === 'ATTACK'){
-        maxDamage = ATTACK_VALUE;
-    } else if (mode === 'STRONG_ATTACK'){
-        maxDamage = STRONG_ATTACK_VALUE;
-    }
-    const damage = dealMonsterDamage(maxDamage);
-    currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-    currentPlayerHealth -= playerDamage;
-    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-        alert('You Won!');
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
-        alert('You Lost!');
-    } else if (currentPlayerHealth <=0 && currentMonsterHealth <=0){
-        alert ('You have a draw!')
-    }
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
 }
 
-
-function attackHandler(){
-    attackHandler('ATTACK');
+function attackHandler() {
+  attackMonster('ATTACK');
 }
 
-function strongAttackHandler(){
-    attackHandler('STRONG_ATTACK');
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
 }
 
-
-attackBtn.addEventListener('click', attackHandler)
-strongAttackBtn.addEventListener('click' , strongAttackHandler)
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
