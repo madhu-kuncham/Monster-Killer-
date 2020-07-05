807eec26d4196bee9e243181bc63c12ca7ee1241
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-07-05 23:44:37 +0530

    for in

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -222,8 +222,13 @@ function printLogHandler (){
     // for (let i =0 ; i < battleLog.length ; i++){
     //     console.log(battleLog);
     // }
+    let i =0;
     for (const logEntry of battleLog){
-        console.log(logEntry);
+        console.log(`#${i}`);       
+        for (const key in logEntry){
+            console.log(`${key} => ${logEntry[key]}`);            
+        }
+        i++;
     }
 }
 
